1a73cf43bc260d4f386b1a98d748dc3df485897c
Dan Shiebler <danshiebler@gmail.com>
2013-12-13 01:44:43 -0500

    Algorithmic optimizations now make updating the books searched and panning over the map far more efficent. Also, major HTML cleanup. More optimizations in the works

--- data/jsonWriting.py
+++ data/jsonWriting.py
@@ -1,7 +1,7 @@
 import  json
 
-myFile = open("JosiahPre1800Locations2-6-13.json")
-bookFile = open("BookTitlesWithLocationsAndYear.json")
+'''myFile = open("JosiahPre1800Locations2-6-13.json")
+#bookFile = open("BookTitlesWithLocationsAndYear.json")
 data = json.load(myFile)
 bookData = json.load(bookFile)
 for location in data['rows']:
@@ -10,9 +10,13 @@ for location in data['rows']:
 		if book['GeoPlace'] == location["Name"]:
 			location['SliderDict'][book["TITLE"][0:10]] = book["Year"]
 myFile.close()
-bookFile.close()
+bookFile.close()'''
 
-with open('JosiahSliderData.json', 'w') as outfile:
-  json.dump(data, outfile)
+with open('JosiahSliderData.json') as infile:
+	with open('JosiahSliderDataTwo.json','w') as outfile:
+		jsonData = json.load(infile)
+		for location in jsonData['rows']:
+			location["bookTitles"] = sorted(location['SliderDict'], key=location['SliderDict'].get)			
+  		json.dump(jsonData,outfile)
 
 #need to write this to a file now
